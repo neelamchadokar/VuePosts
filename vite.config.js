af86385dc76c84c58930cf099fc26edc111bf85f
neelam <neelam@neelams-MacBook-Air.local>
2024-01-15 23:58:37 +0530

    updated build

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
+  base: "/VuePosts/",
   plugins: [vue()],
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/VuePosts/" : "/",
-
+  resolve: {
+    alias: [
+      
+    ]
+  },
 })
